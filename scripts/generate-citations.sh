e845c1257780807aee30e523b508fcc0991202a2
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-09-20 14:10:17 -0400

    Update scripts

--- scripts/generate-citations.sh
+++ scripts/generate-citations.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #Needs an input file http://www.bioinformatics.org/texmed/ using "(((Chakravarty, Mallar[Author]) OR Chakravarty, Mallar M[Author]) OR Chakravarty, M[Author]) OR Chakravarty, M Mallar[Author] "
 #Get Ids
+export PATH=$PATH:~/projects/src/bib2xhtml
 IDS=$(curl -G http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi --data "retmax=100000&db=pubmed&term=(((Chakravarty,+Mallar[Author])+OR+Chakravarty,+Mallar+M[Author])+OR+Chakravarty,+M[Author])+OR+Chakravarty,+M+Mallar[Author]" | grep "<Id>" | sed -r 's/<.{0,1}Id>//g' | tr -d '\t' | paste -sd,)
 
 curl -G http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi --data "db=pubmed&retmode=xml&id=${IDS}" | xsltproc --novalid pubmed2bibtex.xsl - > citations.bib
 
-bib2xhtml -u -r -c -s empty -n Chakravarty citations.bib ../publications/index.html
+bib2xhtml.pl -u -r -c -s empty -n Chakravarty citations.bib ../publications/index.html
 rm citations.bib
+
+dos2unix ../publications/index.html
