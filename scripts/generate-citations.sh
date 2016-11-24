878a276c0c30c5acf1773a55e356b8ce5766c191
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-11-24 16:31:56 -0500

    Path updates for citation script

--- scripts/generate-citations.sh
+++ scripts/generate-citations.sh
@@ -2,9 +2,9 @@
 #Needs an input file http://www.bioinformatics.org/texmed/ using "(((Chakravarty, Mallar[Author]) OR Chakravarty, Mallar M[Author]) OR Chakravarty, M[Author]) OR Chakravarty, M Mallar[Author] "
 #Get Ids
 export PATH=$PATH:~/projects/src/bib2xhtml
-IDS=$(curl -G http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi --data "retmax=100000&db=pubmed&term=(((Chakravarty,+Mallar[Author])+OR+Chakravarty,+Mallar+M[Author])+OR+Chakravarty,+M[Author])+OR+Chakravarty,+M+Mallar[Author]" | grep "<Id>" | sed -r 's/<.{0,1}Id>//g' | tr -d '\t' | paste -sd,)
+IDS=$(curl -G https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi --data "retmax=100000&db=pubmed&term=(((Chakravarty,+Mallar[Author])+OR+Chakravarty,+Mallar+M[Author])+OR+Chakravarty,+M[Author])+OR+Chakravarty,+M+Mallar[Author]" | grep "<Id>" | sed -r 's/<.{0,1}Id>//g' | tr -d '\t' | paste -sd,)
 
-curl -G http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi --data "db=pubmed&retmode=xml&id=${IDS}" | xsltproc --novalid pubmed2bibtex.xsl - > citations.bib
+curl -G https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi --data "db=pubmed&retmode=xml&id=${IDS}" | xsltproc --novalid pubmed2bibtex.xsl - > citations.bib
 
 bib2xhtml.pl -u -r -c -s empty -n Chakravarty citations.bib ../publications/index.html
 rm citations.bib
